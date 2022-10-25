41fe404daff6a8293cad0303b8fe73df64e75547
Natalie <cnapeel@gmail.com>
2022-10-25 15:51:50 -0600

    created helper funcs and started window.onload

--- practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
+++ practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+// cb functions
+
+const createEls = () => {
+    const header = document.createElement('h1');
+    header.innerText = "Kitten Pic"
+    const mainDiv = document.createElement('div');
+    const img = document.createElement("img")
+}
+
+const appendItems = (parent, ...params) => {
+    parent.append(...params);
+}
+
+const fetchImg = async () => {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await response.json();
+    console.log(data)
+    const img = data[0].url;
+    return img;
+}
+
+
+window.onload = () => {
+    document.body.style.backgroundColor = "red";
+    createEls();
+    const img = awfetchImg();
+    appendItems(mainDiv, header, img);
+}
