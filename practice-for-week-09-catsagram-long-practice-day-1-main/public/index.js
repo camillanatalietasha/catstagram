76a417299f3fa2d0123de5640e48cc2965b4f06b
Sohini Bonthala <sohinibonthala@Sohinis-MacBook-Pro.local>
2022-10-25 16:21:40 -0700

    third

--- practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
+++ practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
@@ -1,7 +1,7 @@
 // Your code here
 
 window.onload = async() => {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "skyblue";
     const imgUrl = await fetchImg();
     createEls(imgUrl);
 }
@@ -10,10 +10,39 @@ window.onload = async() => {
 const createEls = (imgUrl) => {
     const header = document.createElement('h1');
     const mainDiv = document.createElement('div');
-    const img = document.createElement("img")
+    const img = document.createElement("img");
+    const imgDiv = document.createElement("div");
+    //set id
+    header.setAttribute("id", "header")
+    mainDiv.setAttribute("id", "mainDiv")
+    img.setAttribute("id", "img")
+    imgDiv.setAttribute("id", "imgDiv")
+
     header.innerText = "Kitten Pic"
     img.setAttribute("src", imgUrl)
-    appendItems(mainDiv, header, img);
+    appendItems(imgDiv, img)
+    appendItems(mainDiv, header, imgDiv);
+
+    const cssFunc = () => {
+        mainDiv.style.display = "flex";
+        mainDiv.style.justifyContent = "center";
+        mainDiv.style.flexDirection = "column";
+        mainDiv.style.alignItems= "center";
+        header.style.margin = "20px";
+        img.style.width = "50%";
+        img.style.height = "50%";
+        imgDiv.style.display = "flex";
+        imgDiv.style.justifyContent = "center";
+        imgDiv.style.alignItems = "center";
+
+        imgDiv.style.width = "800px"
+        imgDiv.style.height = "800px"
+        // img.style.objectFit = "contain"
+        // img.style.padding = "10%"
+
+
+    }
+    cssFunc()
 }
 
 const appendItems = (parent, ...params) => {
